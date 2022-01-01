e1e84a15de1ebe85df06225af53d16e3e07fdf02
Erik Öjebo <erik.ojebo@gmail.com>
2022-01-01 20:47:07 +0100

    More point - polygon collision testing

--- MiniGameSharp/MiniGameSharp.Tests/CollisionDetectorTests.cs
+++ MiniGameSharp/MiniGameSharp.Tests/CollisionDetectorTests.cs
@@ -6,10 +6,13 @@ namespace MiniGameSharp.Tests
 {
     public class CollisionDetectorTests
     {
-        [Test]
-        public void Point_inside_rectangle_collides()
+        private BoundingBoxPolygon _unrotatedRectangle;
+        private BoundingBoxPolygon _triangle;
+
+        [SetUp]
+        public void SetUp()
         {
-            var box = new BoundingBoxPolygon(
+            _unrotatedRectangle = new BoundingBoxPolygon(
                 new[]
                 {
                     new Vector(1, 1),
@@ -19,13 +22,52 @@ namespace MiniGameSharp.Tests
                 }
             );
             
-            AssertCollision(true, new Vector(1.5f, 1.5f), box);
-            AssertCollision(false, new Vector(2.5f, 1.5f), box);
-            AssertCollision(true, new Vector(1f, 1.5f), box);
-            AssertCollision(false, new Vector(2.5f, 2.5f), box);
-            AssertCollision(false, new Vector(1.5f, 2.5f), box);
+            _triangle = new BoundingBoxPolygon(
+                new[]
+                {
+                    new Vector(1, 1),
+                    new Vector(3, 1),
+                    new Vector(2, 3),
+                }
+            );
         }
 
+        [Test]
+        public void Point_inside_rectangle_collides()
+        {
+            AssertCollision(true, new Vector(1.5f, 1.5f), _unrotatedRectangle);
+        }
+
+        [Test]
+        public void Point_with_y_within_rectangle_bounds_but_x_outside_of_bounds_is_not_collision()
+        {
+            AssertCollision(false, new Vector(2.5f, 1.5f), _unrotatedRectangle);
+        }
+        
+        [Test]
+        public void Point_exactly_on_rectangle_edge_collides()
+        {
+            AssertCollision(true, new Vector(1f, 1.5f), _unrotatedRectangle);
+        }
+        
+        [Test]
+        public void Point_with_x_within_rectangle_bounds_but_y_outside_of_bounds_is_not_collission()
+        {
+            AssertCollision(false, new Vector(1.5f, 2.5f), _unrotatedRectangle);
+        }
+        
+        [Test]
+        public void Point_inside_triangle_collides()
+        {
+            AssertCollision(true, new Vector(2, 2), _triangle);
+        }
+
+        [Test]
+        public void Point_outside_triangle_but_inside_rectangular_area_is_not_collision()
+        {
+            AssertCollision(false, new Vector(1.9f, 2.9f), _triangle);
+        }
+        
         private static void AssertCollision(bool expected, Vector point, BoundingBoxPolygon box)
         {
             Assert.AreEqual(expected, CollisionDetector.IsCollision(point, box));
